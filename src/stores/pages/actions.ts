41e1c74a003dd30d8c0488384d8d5923df3eff9c
JiriHusak-lab <56121194+JiriHusak-lab@users.noreply.github.com>
2020-11-11 15:07:46 +0100

    Update actions.ts

--- src/stores/pages/actions.ts
+++ src/stores/pages/actions.ts
@@ -33,10 +33,10 @@ const actions: ActionTree<PagesStore, RootState> = {
       // const params = { roles: ['admin'] };
       const rolesArrStr = role.join(',');
       const queryString = `roles=${rolesArrStr}`;
-      debugger;
+      // debugger;
       const headers = { 'Content-Type': 'text/plain;charset=UTF-8', 'ibm-sec-token': token };
       httpService.getDirect(`${cocoUrl}?${queryString}`, { headers }).then((response: any) => {
-        debugger;
+        // debugger;
         console.log('userPagesData ok case coco ', response);
         // const res: any = response.allowed_content; // server response
         // const res: any = response; // server response
@@ -61,7 +61,7 @@ const actions: ActionTree<PagesStore, RootState> = {
         commit('storePagesData', { allPages, userPages, currentPage });
         resolve(res);
       }, (error: any) => {
-        debugger;
+        // debugger;
         console.log('userPagesData error case coco ', error);
         console.log('error ', error);
         reject(error);
